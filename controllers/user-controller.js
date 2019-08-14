e0cdd38a0ac232d930a0008c1d6263300f41fc44
Rahul Chaudhary <rchaudhary@VELOCIS.IN>
2019-08-14 19:00:03 +0530

    a

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -29,3 +29,11 @@ module.exports.register=function(req,res){
     //   }
     // });
 }
+
+
+function encrypt(text){
+    var cipher = crypto.createCipher('aes-256-cbc','d6f3Efeq');
+    var crypted = cipher.update(text,'utf8','hex')
+    crypted += cipher.final('hex');
+    return crypted;
+}
