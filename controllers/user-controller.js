5bc9f8f3f602ec0bd1e3c039a3db91fb6ac36f17
Rahul Chaudhary <rchaudhary@VELOCIS.IN>
2019-08-23 18:52:06 +0530

    a

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -12,78 +12,46 @@ var connection = require('./../config/config');
 
 module.exports.register=function(req,res){
    
-    var today = new Date();
-    var encryptedString =cryptr.encrypt(req.body.password);
-    var users={
-        "username":req.body.username,
-        "email":req.body.email,
-        "password":encryptedString,
-        "created_at":today
-    }
+                //code if no user with entered email was found
+                var today = new Date();
+                var encryptedString =cryptr.encrypt(req.body.password);
+                var users={
+                "username":req.body.username,
+                "email":req.body.email,
+                "password":encryptedString,
+                "created_at":today
+                }
+
+                connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
+                if (error) {
+
+                //req.flash("error_msg", "there are some error with query.");
+                return res.redirect("userregistration");
+                }else{
+                //on success
+                // req.flash("success_msg", "Registration successfully done.");
+                return res.redirect("userregistration");
+                }
+                });
+          
+
    
-    connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
-      if (error) {
-
-        req.flash("error_msg", "there are some error with query.");
-        return res.redirect("userregistration");
-
-        // res.json({
-        //     status:false,
-        //     message:'there are some error with query'
-        // })
-
-      }else{
-          //on success
-         req.flash("success_msg", "Registration successfully done.");
-        return res.redirect("userregistration");
-
-        //res.render('registration', {user : undefined, success :true,successs :true,error: err});
-         /* res.json({
-            status:true,
-            data:results,
-            message:'user registered sucessfully'
-        })*/
-
-      }
-    });
 }
 
 
-module.exports.register1=function(req,res){
-    console.log("rahul");
-    var today = new Date();
-    var encryptedString =cryptr.encrypt(req.body.password);
-    var users={
-        "username":req.body.username,
-        "email":req.body.email,
-        "password":encryptedString,
-        "created_at":today
-    }
-    console.log(users);
-    connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
-      if (error) {
-
-        req.flash("error_msg", "there are some error with query.");
-        return res.redirect("registration");
-
-        // res.json({
-        //     status:false,
-        //     message:'there are some error with query'
-        // })
-
-      }else{
-          //on success
-         req.flash("success_msg", "Registration successfully done.");
-        return res.redirect("registration");
-
-        //res.render('registration', {user : undefined, success :true,successs :true,error: err});
-         /* res.json({
-            status:true,
-            data:results,
-            message:'user registered sucessfully'
-        })*/
-
-      }
-    });
+module.exports.findRepByEmail=function(req,res){
+           var email=req.body.email;
+           console.log(email);
+             connection.query('SELECT id FROM users WHERE email = ?', email, function(err, rows) {
+            if (err) {
+                return(err, null);
+            } else {
+              console.log(rows[0].id);
+                return(null, rows[0].id);
+                }
+        });
 }
+
+
+
 
--- routes/pages.js
+++ routes/pages.js
@@ -20,21 +20,37 @@ router.get("/userregistration",function(req,res){
 
 
 router.post('/userregistration', 
-	[
-    check('username').not().isEmpty().withMessage('User Name field is required'),
-    check('email').not().isEmpty().withMessage('Email Id field is required'),
-    check('password').not().isEmpty().withMessage('Password field is required'),
-    check('password_confirm').not().isEmpty().withMessage('Confirm Password field is required'),
+	// [
+ //    check('username').not().isEmpty().withMessage('User Name field is required'),
+ //    check('email').not().isEmpty().withMessage('Email Id field is required'),
+ //    check('password').not().isEmpty().withMessage('Password field is required'),
+ //    check('password_confirm').not().isEmpty().withMessage('Confirm Password field is required'),
     
-	],
-	function(req, res){
-		const errors = validationResult(req);
-		if(!errors.isEmpty()){
-			 res.render('registration', {fulldata:req.body,page:'User Registration', menuId:'registration', error: errors.mapped()});
-		}else{
-			userController.register(req,res);
-		}	
-	}
+	// ],
+	// function(req, res){
+	// 	const errors = validationResult(req);
+	// 	if(!errors.isEmpty()){
+	// 		 res.render('registration', {fulldata:req.body,page:'User Registration', menuId:'registration', error: errors.mapped()});
+	// 	}else{
+		check('email').custom((value, { req }) => {
+      return new Promise((resolve, reject) => {
+        userController.findRepByEmail({ 'email': value }, (err, rep) => {
+            if(req.params.rep_id){
+              return resolve();
+            }else{
+            if(rep[0].count > 0) {
+                return reject();
+            } else {
+                return resolve();
+            }
+          }
+         });
+      });
+   }).withMessage('This email is already in use'),
+
+	userController.register
+	// 	}	
+	// }
 	
 );
 
